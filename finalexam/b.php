3542bb43ee001d9e3aa59b079b92c9100317e633
hishamalip <hishamalip@gmail.com>
2019-05-16 15:04:05 +0530

    small changes

--- finalexam/b.php
+++ finalexam/b.php
@@ -1,28 +1,37 @@
 <?php
 $a1 = $_POST['q1'];
 if ($a1 == "Antoine Lavoisier")
+	{
 	echo "1 is correcty answer";
-	echo "<br>";
-else
+	echo "<br>";	
+	}
+else	
+	{
 	echo "1 is wrong";
 	echo "<br>";
+	}
 
 $a2 = $_POST['q2'];
 if ($a2 == "Dmitri Mendeleev")
+	{
 	echo "2 is correcty answer";
 	echo "<br>";
-
+	}
 else
+	{
 	echo "2 is wrong";
 	echo "<br>";
+	}
 
 $a3 = $_POST['q3'];
-if ($a1 == "Sulphur dioxide")
+if ($a3 == "Sulphur dioxide")
+	{
 	echo "3 is correcty answer";
 	echo "<br>";
-
+	}
 else
+	{
 	echo "3 is wrong";
 	echo "<br>";
-
+	}
 ?>
